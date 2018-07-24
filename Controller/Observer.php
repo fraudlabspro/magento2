9c1a8f3e95721715a9358e13e5874f26670217dd
fraudlabspro <support@fraudlabspro.com>
2018-07-24 09:35:22 +0800

    fix compatibility issue

--- Controller/Observer.php
+++ Controller/Observer.php
@@ -128,7 +128,7 @@ class Observer implements ObserverInterface {
             'payment_mode' => $paymentMode,
             'flp_checksum' => ( isset( $_COOKIE['flp_checksum'] ) ) ? $_COOKIE['flp_checksum'] : '',
             'source' => 'magento',
-            'source_version' => '2.0.10',
+            'source_version' => '2.0.11',
         );
 
         $shippingAddress = $order->getShippingAddress();
