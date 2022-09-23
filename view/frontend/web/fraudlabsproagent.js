d9fd35485587bad7a782ec16387bff8410cd7daf
FraudLabs Pro <support@fraudlabspro.com>
2022-09-23 12:40:27 +0800

    Fixed missing device information issue

--- view/frontend/web/fraudlabsproagent.js
+++ view/frontend/web/fraudlabsproagent.js
@@ -1,4 +1,14 @@
 (function () {
+    function s() {
+        var e = document.createElement('script');
+        e.type = 'text/javascript';
+        e.async = true;
+        e.src = ('https:' === document.location.protocol ? 'https://' : 'http://') + 'cdn.fraudlabspro.com/s.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(e, s);
+    }
+    (window.attachEvent) ? window.attachEvent('onload', s) : window.addEventListener('load', s, false);
+
     function random(length) {
         var key = '';
         var patterns = '0123456789abcdefghijklmnopqrstuvwxyz';
